e63608cffc5ab3657a3a0eaaa30746c09066a19b
Alexander Kirk <alexanderjoshua21@protonmail.com>
2026-03-31 11:53:53 +0100

    Updated documentation

---
+++ provision-vm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y git ansible ca-certificates curl
+git clone https://github.com/ErcModel3/Aquila.git
+git clone https://github.com/srl-labs/vrnetlab.git
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+bash -c "$(curl -sL https://get.containerlab.dev)"
